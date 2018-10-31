2ca3f75ed694bbbe5468b5fe4945709a5e2aadb4
markdady <ymsdu2004@gmail.com>
2018-10-31 14:29:51 +0800

    Update test.py

--- demo/test.py
+++ demo/test.py
@@ -3,7 +3,7 @@ import sys
 import time
 
 if __name__ == '__main__':
-    for i in range(1000):
+    for i in range(10):
         sys.stderr.write("\nvalidation at step 100\n")
         print('hello')
         time.sleep(10)
