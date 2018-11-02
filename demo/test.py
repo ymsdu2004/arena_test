9a4291aa0a2c4b7142d7765d5310f205405a3f62
markdady <ymsdu2004@gmail.com>
2018-11-02 14:36:25 +0800

    Update test.py

--- demo/test.py
+++ demo/test.py
@@ -3,6 +3,7 @@ import sys
 import time
 
 if __name__ == '__main__':
+    os.system("ls /gruntdata")
     for i in range(10):
         sys.stderr.write("\nvalidation at step 100\n")
         print('hello')
